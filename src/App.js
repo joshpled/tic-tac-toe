ea35aae0ad6a1ff00b3505701b6db6f16584c712
Joshua Perez Leduc <joshuapleduc@gmail.com>
2021-05-16 15:26:17 -0400

    add styling to background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import Game from "./components/Game";
 const style = {
-  marginTop: '-5em',
-}
+  marginLeft: "auto",
+  marginRight: "auto",
+  borderStyle: "solid",
+  width: "30%",
+  height:'100vh',
+};
 function App() {
   const [newGame, setNewGame] = useState(false);
   return (
-    <div style={style}>
-      <Game />
-    </div>
+    <>
+      <div style={style} className="game-board">
+        <Game />
+      </div>
+    </>
   );
 }
 
